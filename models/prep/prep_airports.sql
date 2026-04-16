064eb0f4e94444e66d7b3d16279acee2f8549568
giladgo1 <giladgo1@gmail.com>
2026-04-16 16:05:01 +0200

    airports order

---
+++ models/prep/prep_airports.sql
@@ -0,0 +1,6 @@
+SELECT * from staging_airports;
+WITH airports_reorder AS (
+        SELECT faa, name, lat, lon, alt, tz, dst, city, country, region
+        	         FROM {{ref('staging_airports')}}
+    )
+    SELECT * FROM airports_reorder
